a4988aca5feb4a5f7d77c99009d00d8dd4413828
christiang@omniaccounts.co.za <ChristianCKS>
2020-01-11 06:59:52 +0200

    Daily backups - 11/01/2020

--- BaseGrid_Frm.pas
+++ BaseGrid_Frm.pas
@@ -5,7 +5,7 @@ interface
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, Vcl.Forms,
   System.Classes, Vcl.Graphics, Vcl.Controls, Vcl.Dialogs, System.IOUtils,
-  WinApi.ShellApi,
+  System.Win.Registry, WinApi.ShellApi,
 
   BaseLayout_Frm, CommonValues, VBCommonValues,
 
@@ -21,7 +21,7 @@ uses
   cxNavigator, dxDateRanges, Data.DB, cxDBData, cxDBNavigator, cxGridLevel,
   cxGridCustomView, cxGridCustomTableView, cxGridTableView, cxGridBandedTableView,
   cxGridDBBandedTableView, cxGrid, dxScrollbarAnnotations, dxPrnDev, dxPrnDlg,
-  cxGridExportLink;
+  cxGridExportLink, dxLayoutcxEditAdapters, cxContainer, cxCheckBox;
 
 type
   TBaseGridFrm = class(TBaseLayoutFrm)
@@ -35,8 +35,8 @@ type
     grpToolbar: TdxLayoutGroup;
     dlgFileSave: TSaveDialog;
     dlgPrint: TdxPrintDialog;
-    procedure ExportToExcel(FileName: string; Grid: TcxGrid);
-    procedure ExportToPDF(FileName: string; Grid: TcxGrid);
+    litOpenAfterExport: TdxLayoutItem;
+    cbxOpenAfterExport: TcxCheckBox;
 
     procedure viewMasterCustomDrawCell(Sender: TcxCustomGridTableView;
       ACanvas: TcxCanvas; AViewInfo: TcxGridTableDataCellViewInfo; var ADone: Boolean);
@@ -65,6 +65,8 @@ implementation
 
 uses
   CommonFunction,
+  VBBase_DM,
+  MT_DM,
   Report_DM;
 
 procedure TBaseGridFrm.DrawCellBorder(var Msg: TMessage);
@@ -75,9 +77,23 @@ begin
 end;
 
 procedure TBaseGridFrm.FormCreate(Sender: TObject);
+var
+  RegKey: TRegistry;
 begin
   inherited;
   ReportDM.PrintExporting := False;
+  RegKey := TRegistry.Create(KEY_ALL_ACCESS or KEY_WRITE or KEY_WOW64_64KEY);
+  RegKey.RootKey := HKEY_CURRENT_USER;
+  try
+    RegKey.OpenKey(KEY_MASTER_TABLE_MANAGER, True);
+
+    if not RegKey.ValueExists('Open Ducoment After Export') then
+      RegKey.WriteBool('Open Ducoment After Export', True);
+
+    cbxOpenAfterExport.Checked := RegKey.ReadBool('Open Ducoment After Export');
+  finally
+    RegKey.Free;
+  end;
 end;
 
 procedure TBaseGridFrm.FormShow(Sender: TObject);
@@ -89,165 +105,206 @@ end;
 
 procedure TBaseGridFrm.navMasterButtonsButtonClick(Sender: TObject;
   AButtonIndex: Integer; var ADone: Boolean);
+var
+  ID: Integer;
 begin
   inherited;
 //  // Don't allow printing or exporting data whilst editing data.
 //  if (AButtonIndex in [16, 17, 18, 19]) and (navMaster.DataSet.State in [dsEdit, dsInsert]) then
 //    raise EExecutionException.Create('Cannot use the print/export functions whilst editing data.' + CRLF +
 //      'Please post or cancel the current transaction and try again.');
-
   case AButtonIndex of
+    NBDI_REFRESH:
+      begin
+        ADone := True;
+        case ReportDM.MasterFormType of
+          ftActivityType:
+            begin
+              ID := MTDM.cdsActivityType.FieldByName('ID').AsInteger;
+
+              VBBaseDM.GetData(39, MTDM.cdsActivityType, MTDM.cdsActivityType.Name, '',
+                'C:\Data\Xml\Activity Type.xml', MTDM.cdsActivityType.UpdateOptions.Generatorname,
+                MTDM.cdsActivityType.UpdateOptions.UpdateTableName);
+
+              if not MTDM.cdsActivityType.Locate('ID', ID, []) then
+                MTDM.cdsActivityType.First;
+            end;
+          ftAgePeriod:
+            begin
+              ID := MTDM.cdsAgePeriod.FieldByName('ID').AsInteger;
+
+              VBBaseDM.GetData(13, MTDM.cdsAgePeriod, MTDM.cdsAgePeriod.Name, '',
+                'C:\Data\Xml\Age Period.xml', MTDM.cdsAgePeriod.UpdateOptions.Generatorname,
+                MTDM.cdsAgePeriod.UpdateOptions.UpdateTableName);
+
+              if not MTDM.cdsAgePeriod.Locate('ID', ID, []) then
+                MTDM.cdsAgePeriod.First;
+            end;
+
+          ftBankAccountType:
+            begin
+              ID := MTDM.cdsBankAccountType.FieldByName('ID').AsInteger;
+
+              VBBaseDM.GetData(6, MTDM.cdsBankAccountType, MTDM.cdsBankAccountType.Name, '',
+                'C:\Data\Xml\Bannk Account Type.xml', MTDM.cdsBankAccountType.UpdateOptions.Generatorname,
+                MTDM.cdsBankAccountType.UpdateOptions.UpdateTableName);
+
+              if not MTDM.cdsBankAccountType.Locate('ID', ID, []) then
+                MTDM.cdsBankAccountType.First;
+            end;
+          ftBank:
+            begin
+              ID := MTDM.cdsBank.FieldByName('ID').AsInteger;
+
+              VBBaseDM.GetData(5, MTDM.cdsBank, MTDM.cdsBank.Name, '',
+                'C:\Data\Xml\Bannk.xml', MTDM.cdsBank.UpdateOptions.Generatorname,
+                MTDM.cdsBank.UpdateOptions.UpdateTableName);
+
+              if not MTDM.cdsBank.Locate('ID', ID, []) then
+                MTDM.cdsBank.First;
+            end;
+          ftContactType:
+            begin
+              ID := MTDM.cdsContactType.FieldByName('ID').AsInteger;
+
+              VBBaseDM.GetData(11, MTDM.cdsContactType, MTDM.cdsContactType.Name, '',
+                'C:\Data\Xml\Age Period.xml', MTDM.cdsContactType.UpdateOptions.Generatorname,
+                MTDM.cdsContactType.UpdateOptions.UpdateTableName);
+
+              if not MTDM.cdsContactType.Locate('ID', ID, []) then
+                MTDM.cdsContactType.First;
+            end;
+          ftCountry:
+            begin
+              ID := MTDM.cdsCountry.FieldByName('ID').AsInteger;
+
+              VBBaseDM.GetData(12, MTDM.cdsCountry, MTDM.cdsCountry.Name, '',
+                'C:\Data\Xml\Age Period.xml', MTDM.cdsCountry.UpdateOptions.Generatorname,
+                MTDM.cdsCountry.UpdateOptions.UpdateTableName);
+
+              if not MTDM.cdsCountry.Locate('ID', ID, []) then
+                MTDM.cdsCountry.First;
+            end;
+          ftCustomerGroup:
+            begin
+              ID := MTDM.cdsCustomerGroup.FieldByName('ID').AsInteger;
+
+              VBBaseDM.GetData(56, MTDM.cdsCustomerGroup, MTDM.cdsCustomerGroup.Name, '',
+                'C:\Data\Xml\Age Period.xml', MTDM.cdsCustomerGroup.UpdateOptions.Generatorname,
+                MTDM.cdsCustomerGroup.UpdateOptions.UpdateTableName);
+
+              if not MTDM.cdsCustomerGroup.Locate('ID', ID, []) then
+                MTDM.cdsCustomerGroup.First;
+            end;
+          ftCustomerStatus:
+            begin
+              ID := MTDM.cdsCustomerStatus.FieldByName('ID').AsInteger;
+
+              VBBaseDM.GetData(14, MTDM.cdsCustomerStatus, MTDM.cdsCustomerStatus.Name, '',
+                'C:\Data\Xml\Age Period.xml', MTDM.cdsCustomerStatus.UpdateOptions.Generatorname,
+                MTDM.cdsCustomerStatus.UpdateOptions.UpdateTableName);
+
+              if not MTDM.cdsCustomerStatus.Locate('ID', ID, []) then
+                MTDM.cdsCustomerStatus.First;
+            end;
+          ftCustomerType:
+            begin
+              ID := MTDM.cdsCustomerType.FieldByName('ID').AsInteger;
+
+              VBBaseDM.GetData(15, MTDM.cdsCustomerType, MTDM.cdsCustomerType.Name, '',
+                'C:\Data\Xml\Age Period.xml', MTDM.cdsCustomerType.UpdateOptions.Generatorname,
+                MTDM.cdsCustomerType.UpdateOptions.UpdateTableName);
+
+              if not MTDM.cdsCustomerType.Locate('ID', ID, []) then
+                MTDM.cdsCustomerType.First;
+            end;
+          ftJobFunction:
+            begin
+              ID := MTDM.cdsJobFunction.FieldByName('ID').AsInteger;
+
+              VBBaseDM.GetData(19, MTDM.cdsJobFunction, MTDM.cdsJobFunction.Name, '',
+                'C:\Data\Xml\Age Period.xml', MTDM.cdsJobFunction.UpdateOptions.Generatorname,
+                MTDM.cdsJobFunction.UpdateOptions.UpdateTableName);
+
+              if not MTDM.cdsJobFunction.Locate('ID', ID, []) then
+                MTDM.cdsJobFunction.First;
+            end;
+          ftMonthOfYear:
+            begin
+              ID := MTDM.cdsMonthOfYear.FieldByName('ID').AsInteger;
+
+              VBBaseDM.GetData(20, MTDM.cdsMonthOfYear, MTDM.cdsMonthOfYear.Name, '',
+                'C:\Data\Xml\Age Period.xml', MTDM.cdsMonthOfYear.UpdateOptions.Generatorname,
+                MTDM.cdsMonthOfYear.UpdateOptions.UpdateTableName);
+
+              if not MTDM.cdsMonthOfYear.Locate('ID', ID, []) then
+                MTDM.cdsMonthOfYear.First;
+            end;
+          ftRateUnit:
+            begin
+              ID := MTDM.cdsRateUnit.FieldByName('ID').AsInteger;
+
+              VBBaseDM.GetData(38, MTDM.cdsRateUnit, MTDM.cdsRateUnit.Name, '',
+                'C:\Data\Xml\Age Period.xml', MTDM.cdsRateUnit.UpdateOptions.Generatorname,
+                MTDM.cdsRateUnit.UpdateOptions.UpdateTableName);
+
+              if not MTDM.cdsRateUnit.Locate('ID', ID, []) then
+                MTDM.cdsRateUnit.First;
+            end;
+          ftSalutation:
+            begin
+              ID := MTDM.cdsSalutation.FieldByName('ID').AsInteger;
+
+              VBBaseDM.GetData(23, MTDM.cdsSalutation, MTDM.cdsSalutation.Name, '',
+                'C:\Data\Xml\Age Period.xml', MTDM.cdsSalutation.UpdateOptions.Generatorname,
+                MTDM.cdsSalutation.UpdateOptions.UpdateTableName);
+
+              if not MTDM.cdsSalutation.Locate('ID', ID, []) then
+                MTDM.cdsSalutation.First;
+            end;
+          ftStdActivity:
+            begin
+              ID := MTDM.cdsStdActivity.FieldByName('ID').AsInteger;
+
+              VBBaseDM.GetData(52, MTDM.cdsStdActivity, MTDM.cdsStdActivity.Name, '',
+                'C:\Data\Xml\Age Period.xml', MTDM.cdsStdActivity.UpdateOptions.Generatorname,
+                MTDM.cdsStdActivity.UpdateOptions.UpdateTableName);
+
+              if not MTDM.cdsStdActivity.Locate('ID', ID, []) then
+                MTDM.cdsStdActivity.First;
+            end;
+          ftTaxoffice:
+            begin
+              ID := MTDM.cdsTaxOffice.FieldByName('ID').AsInteger;
+
+              VBBaseDM.GetData(25, MTDM.cdsTaxOffice, MTDM.cdsTaxOffice.Name, '',
+                'C:\Data\Xml\Age Period.xml', MTDM.cdsTaxOffice.UpdateOptions.Generatorname,
+                MTDM.cdsTaxOffice.UpdateOptions.UpdateTableName);
+
+              if not MTDM.cdsTaxOffice.Locate('ID', ID, []) then
+                MTDM.cdsTaxOffice.First;
+            end;
+          ftVehicleMake:
+            begin
+              ID := MTDM.cdsVehicle.FieldByName('ID').AsInteger;
+
+              VBBaseDM.GetData(48, MTDM.cdsVehicle, MTDM.cdsVehicle.Name, '',
+                'C:\Data\Xml\Age Period.xml', MTDM.cdsVehicle.UpdateOptions.Generatorname,
+                MTDM.cdsVehicle.UpdateOptions.UpdateTableName);
+
+              if not MTDM.cdsVehicle.Locate('ID', ID, []) then
+                MTDM.cdsVehicle.First;
+            end;
+        end;
+      end;
+
     16: ReportDM.ReportAction := raPreview;
     17: ReportDM.ReportAction := raPrint;
     18: ReportDM.ReportAction := raExcel;
     19: ReportDM.ReportAction := raPDF;
   end;
+
   SetPrintButtonStatus(TFDMemTable(navMaster.DataSet));
-
-//  case ReportDM.ReportAction of
-//    raPreview, raPrint, raExcel, raPDF:
-//      begin
-//        Screen.Cursor := crHourglass;
-//        ReportDM.PrintExporting := True;
-//        try
-//          ReportTypeName := 'Activity Type Listing';
-//          case ReportDM.ReportAction of
-//            raPreview, raPrint:
-//              begin
-//                RepFileName := MTDM.ShellResource.ReportFolder + 'MasterGenericTableTemplate.fr3';
-//
-//                if not TFile.Exists(RepFileName) then
-//                  raise EFileNotFoundException.Create('Report file: ' + RepFileName + ' not found. Cannot load report.');
-//
-//                PrintExportReport := TPrintExportData.Create;
-//                PrintExportReport.SourceDataSet := MTDM.cdsActivityType;
-//                PrintExportReport.TargetDataSet := ReportDM.cdsActivityType;
-//                PrintExportReport.Report := ReportDM.rptMaster;
-//                PrintExportReport.ReportDataSet := ReportDM.fdsMaster;
-//                PrintExportReport.ReportTypeName := ReportTypeName;
-//                PrintExportReport.ReportFileName := RepFileName;
-//                PrintExportReport.ReportAction := ReportDM.ReportAction;
-//                PrintExportReport.PrintPreview;
-//              end;
-//
-//            raExcel:
-//              begin
-//                ExportToExcel(ReportTypeName, grdMaster);
-//              end;
-//
-//            raPDF:
-//              begin
-//
-//              end;
-//          end;
-//        finally
-//          PrintExportReport.Free;
-//          ReportDM.PrintExporting := False;
-//          Screen.Cursor := crDefault;
-//        end;
-//      end;
-//  end;
-end;
-
-procedure TBaseGridFrm.ExportToExcel(FileName: string; Grid: TcxGrid);
-var
-  DestFolder, FolderPath, ExportFileName: string;
-  FileSaved: Boolean;
-  RepFileName: string;
-//  ProgressDialog: TExcelExportProgressFrm;
-begin
-  inherited;
-  FolderPath := EXCEL_DOCS;
-//  FolderPath := MainFrm.FShellResource.RootFolder + '\' + FSHIFT_FOLDER + 'Export\';
-  TDirectory.CreateDirectory(FolderPath);
-  dlgFileSave.DefaultExt := 'xlsx';
-  dlgFileSave.InitialDir := FolderPath;
-//  dlgFileSave.FileName := '*.xlsx';
-  dlgFileSave.FileName := Filename + '.xlsx';
-  FileSaved := dlgFileSave.Execute;
-
-  if not FileSaved then
-    Exit;
-
-//  if dlgFileSave.Execute then
-  if TFile.Exists(dlgFileSave.FileName) then
-  begin
-    Beep;
-    if DisplayMsg(Application.Title,
-      'File Overwrite',
-      'The file ' + dlgFileSave.FileName + ' already exists.' + CRLF +
-      'Do you want to overwrite this file?',
-      mtConfirmation,
-      [mbYes, mbNo]
-      ) = mrNo then
-      Exit;
-  end;
-
-  ExportFileName := dlgFileSave.FileName;
-  ExportGridToXLSX(
-    ExportFileName, // Filename to export
-    Grid, // Grid whose data must be exported
-    True, // Expand groups
-    True, // Save all records (Selected and un-selected ones)
-    True, // Use native format
-    'xlsx');
-
-//    if cbxOepnDocument.Checked then
-  ShellExecute(0, 'open', PChar('Excel.exe'), PChar('"' + ExportFileName + '"'), nil, SW_SHOWNORMAL)
-end;
-
-procedure TBaseGridFrm.ExportToPDF(FileName: string; Grid: TcxGrid);
-var
-  FileSaved: Boolean;
-  DC: TcxCustomDataController;
-  RepFileName: string;
-begin
-  inherited;
-  ReportDM.frxPDFExport.ShowDialog := False;
-  ReportDM.frxPDFExport.Background := True;
-  ReportDM.frxPDFExport.OpenAfterExport := True; //cbxOepnDocument.Checked;
-  ReportDM.frxPDFExport.OverwritePrompt := True;
-  ReportDM.frxPDFExport.ShowProgress := True;
-//  TfrxGroupHeader(ReportDM.rptBillableSummaryByCustomer.FindObject('bndCustomerHeader')).Visible := False;
-//  TfrxMemoView(ReportDM.rptBillableSummaryByCustomer.FindObject('lblCustomerHeader')).Visible := False;
-  dlgFileSave.DefaultExt := 'pdf';
-  dlgFileSave.InitialDir := PDF_DOCS;
-  dlgFileSave.FileName := '*.pdf';
-
-  FileSaved := dlgFileSave.Execute;
-
-  if not FileSaved then
-    Exit;
-
-  if TFile.Exists(dlgFileSave.FileName) then
-  begin
-    Beep;
-    if DisplayMsg(Application.Title,
-      'File Overwrite',
-      'The file ' + dlgFileSave.FileName + ' already exists. Do you want to overwrite this file?',
-      mtConfirmation,
-      [mbYes, mbNo]
-      ) = mrNo then
-      Exit;
-  end;
-
-//  RepFileName := TSDM.ShellResource.ReportFolder + FReportFileName[grpData.ItemIndex];
-//
-//  if not TFile.Exists(RepFileName) then
-//    raise EFileNotFoundException.Create('Report file: ' + RepFileName + ' not found. Cannot load report.');
-//
-//  ReportDM.FReport.LoadFromFile(TSDM.ShellResource.ReportFolder + FReportFileName[grpData.ItemIndex]);
-
-//  DC := viewTimesheetBillable.DataController;
-//  DC.BeginUpdate;
-//  try
-//    ReportDM.frxPDFExport.FileName := dlgFileSave.FileName;
-//    if ReportDM.FReport.PrepareReport(True) then
-//      ReportDM.FReport.Export(ReportDM.frxPDFExport);
-//  finally
-//    ReportDM.cdsTSBillable.First;
-//    DC.EndUpdate;
-//  end;
 end;
 
 procedure TBaseGridFrm.SetButtonVisibility(DataSet: TFDMemTable; MasterID: Integer);
