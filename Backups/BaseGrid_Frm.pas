4dd5cea6af0a7c651fe4192b338c19f48cb76537
christiang@omniaccounts.co.za <ChristianCKS>
2020-01-15 02:46:52 +0200

    Daily backups - 15-01-2020

--- Backups/BaseGrid_Frm.pas
+++ Backups/BaseGrid_Frm.pas
@@ -25,23 +25,23 @@ uses
 
 type
   TBaseGridFrm = class(TBaseLayoutFrm)
-    imgNav: TcxImageList;
-    dlgFileSave: TSaveDialog;
-    dlgPrint: TdxPrintDialog;
-    grpToolbar: TdxLayoutGroup;
-    litNavigator: TdxLayoutItem;
-    litOpenAfterExport: TdxLayoutItem;
-    litGrid: TdxLayoutItem;
     grdMaster: TcxGrid;
     viewMaster: TcxGridDBBandedTableView;
     lvlMaster: TcxGridLevel;
     navMaster: TcxDBNavigator;
+    imgNav: TcxImageList;
+    litNavigator: TdxLayoutItem;
+    litGrid: TdxLayoutItem;
+    grpToolbar: TdxLayoutGroup;
+    dlgFileSave: TSaveDialog;
+    dlgPrint: TdxPrintDialog;
+    litOpenAfterExport: TdxLayoutItem;
     cbxOpenAfterExport: TcxCheckBox;
-    procedure DrawCellBorder(var Msg: TMessage); message CM_DRAWBORDER;
 
-    procedure FormCreate(Sender: TObject);
     procedure viewMasterCustomDrawCell(Sender: TcxCustomGridTableView;
-      ACanvas: TcxCanvas; AViewInfo: TcxGridTableDataCellViewInfo;      var ADone: Boolean);
+      ACanvas: TcxCanvas; AViewInfo: TcxGridTableDataCellViewInfo; var ADone: Boolean);
+    procedure DrawCellBorder(var Msg: TMessage); message CM_DRAWBORDER;
+//    procedure SetButtonVisibility(ReadOnlyDataSet: Boolean);
     procedure SetButtonVisibility(DataSet: TFDMemTable; MasterID: Integer);
     procedure SetPrintButtonStatus(DataSet: TFDMemTable);
     procedure FormShow(Sender: TObject);
@@ -49,8 +49,10 @@ type
       AButtonIndex: Integer; var ADone: Boolean);
   private
     { Private declarations }
+//    FMasterFormType: TMasterFormTypes;
   public
     { Public declarations }
+//    property MasterFormType: TMasterFormTypes read FMasterFormType write FMasterFormType;
   end;
 
 var
@@ -71,26 +73,6 @@ begin
   if (TObject(Msg.WParam) is TcxCanvas)
     and (TObject(Msg.LParam) is TcxGridTableDataCellViewInfo) then
     TcxCanvas(Msg.WParam).DrawComplexFrame(TcxGridTableDataCellViewInfo(Msg.LParam).ClientBounds, clRed, clRed, cxBordersAll, 1);
-end;
-
-procedure TBaseGridFrm.FormCreate(Sender: TObject);
-var
-  RegKey: TRegistry;
-begin
-  inherited;
-  ReportDM.PrintExporting := False;
-  RegKey := TRegistry.Create(KEY_ALL_ACCESS or KEY_WRITE or KEY_WOW64_64KEY);
-  RegKey.RootKey := HKEY_CURRENT_USER;
-  try
-    RegKey.OpenKey(KEY_MASTER_TABLE_MANAGER, True);
-
-    if not RegKey.ValueExists('Open Ducoment After Export') then
-      RegKey.WriteBool('Open Ducoment After Export', True);
-
-    cbxOpenAfterExport.Checked := RegKey.ReadBool('Open Ducoment After Export');
-  finally
-    RegKey.Free;
-  end;
 end;
 
 procedure TBaseGridFrm.FormShow(Sender: TObject);
@@ -129,12 +111,30 @@ begin
                 DataSet := MTDM.cdsActivityType;
                 ScriptID := 39;
                 FileName := 'C:\Data\Xml\Activity Type.xml';
+
+//              ID := MTDM.cdsActivityType.FieldByName('ID').AsInteger;
+//
+//              VBBaseDM.GetData(39, MTDM.cdsActivityType, MTDM.cdsActivityType.Name, '',
+//                'C:\Data\Xml\Activity Type.xml', MTDM.cdsActivityType.UpdateOptions.Generatorname,
+//                MTDM.cdsActivityType.UpdateOptions.UpdateTableName);
+//
+//              if not MTDM.cdsActivityType.Locate('ID', ID, []) then
+//                MTDM.cdsActivityType.First;
               end;
             ftAgePeriod:
               begin
                 DataSet := MTDM.cdsAgePeriod;
                 ScriptID := 13;
                 FileName := 'C:\Data\Xml\Age Period.xml';
+
+//              ID := MTDM.cdsAgePeriod.FieldByName('ID').AsInteger;
+//
+//              VBBaseDM.GetData(13, MTDM.cdsAgePeriod, MTDM.cdsAgePeriod.Name, '',
+//                'C:\Data\Xml\Age Period.xml', MTDM.cdsAgePeriod.UpdateOptions.Generatorname,
+//                MTDM.cdsAgePeriod.UpdateOptions.UpdateTableName);
+//
+//              if not MTDM.cdsAgePeriod.Locate('ID', ID, []) then
+//                MTDM.cdsAgePeriod.First;
               end;
 
             ftBank:
@@ -142,6 +142,15 @@ begin
                 DataSet := MTDM.cdsBank;
                 ScriptID := 5;
                 FileName := 'C:\Data\Xml\Bank.xml';
+
+//              ID := MTDM.cdsBank.FieldByName('ID').AsInteger;
+//
+//              VBBaseDM.GetData(5, MTDM.cdsBank, MTDM.cdsBank.Name, '',
+//                'C:\Data\Xml\Bannk.xml', MTDM.cdsBank.UpdateOptions.Generatorname,
+//                MTDM.cdsBank.UpdateOptions.UpdateTableName);
+//
+//              if not MTDM.cdsBank.Locate('ID', ID, []) then
+//                MTDM.cdsBank.First;
               end;
 
             ftBankAccountType:
@@ -149,6 +158,15 @@ begin
                 DataSet := MTDM.cdsBankAccountType;
                 ScriptID := 39;
                 FileName := 'C:\Data\Xml\Bank Account Type.xml';
+
+//              ID := MTDM.cdsBankAccountType.FieldByName('ID').AsInteger;
+//
+//              VBBaseDM.GetData(6, MTDM.cdsBankAccountType, MTDM.cdsBankAccountType.Name, '',
+//                'C:\Data\Xml\Bannk Account Type.xml', MTDM.cdsBankAccountType.UpdateOptions.Generatorname,
+//                MTDM.cdsBankAccountType.UpdateOptions.UpdateTableName);
+//
+//              if not MTDM.cdsBankAccountType.Locate('ID', ID, []) then
+//                MTDM.cdsBankAccountType.First;
               end;
 
             ftContactType:
@@ -156,6 +174,15 @@ begin
                 DataSet := MTDM.cdsContactType;
                 ScriptID := 11;
                 FileName := 'C:\Data\Xml\Contact Type.xml';
+
+//              ID := MTDM.cdsContactType.FieldByName('ID').AsInteger;
+//
+//              VBBaseDM.GetData(11, MTDM.cdsContactType, MTDM.cdsContactType.Name, '',
+//                'C:\Data\Xml\Age Period.xml', MTDM.cdsContactType.UpdateOptions.Generatorname,
+//                MTDM.cdsContactType.UpdateOptions.UpdateTableName);
+//
+//              if not MTDM.cdsContactType.Locate('ID', ID, []) then
+//                MTDM.cdsContactType.First;
               end;
 
             ftCountry:
@@ -163,6 +190,15 @@ begin
                 DataSet := MTDM.cdsCountry;
                 ScriptID := 12;
                 FileName := 'C:\Data\Xml\Country.xml';
+
+//              ID := MTDM.cdsCountry.FieldByName('ID').AsInteger;
+//
+//              VBBaseDM.GetData(12, MTDM.cdsCountry, MTDM.cdsCountry.Name, '',
+//                'C:\Data\Xml\Age Period.xml', MTDM.cdsCountry.UpdateOptions.Generatorname,
+//                MTDM.cdsCountry.UpdateOptions.UpdateTableName);
+//
+//              if not MTDM.cdsCountry.Locate('ID', ID, []) then
+//                MTDM.cdsCountry.First;
               end;
 
             ftCustomer:
@@ -170,6 +206,15 @@ begin
                 DataSet := MTDM.cdsCustomer;
                 ScriptID := 3;
                 FileName := 'C:\Data\Xml\Customer.xml';
+
+//              ID := MTDM.cdsCustomer.FieldByName('ID').AsInteger;
+//
+//              VBBaseDM.GetData(3, MTDM.cdsCustomer, MTDM.cdsCustomer.Name, ONE_SPACE,
+//                'C:\Data\Xml\Customer.xml', MTDM.cdsCustomer.UpdateOptions.Generatorname,
+//                MTDM.cdsCustomer.UpdateOptions.UpdateTableName);
+//
+//              if not MTDM.cdsCustomer.Locate('ID', ID, []) then
+//                MTDM.cdsCustomer.First;
               end;
 
             ftCustomerGroup:
@@ -177,12 +222,31 @@ begin
                 DataSet := MTDM.cdsCustomerGroup;
                 ScriptID := 56;
                 FileName := 'C:\Data\Xml\Customer Group.xml';
+
+//              ID := MTDM.cdsCustomerGroup.FieldByName('ID').AsInteger;
+//
+//              VBBaseDM.GetData(56, MTDM.cdsCustomerGroup, MTDM.cdsCustomerGroup.Name, '',
+//                'C:\Data\Xml\Age Period.xml', MTDM.cdsCustomerGroup.UpdateOptions.Generatorname,
+//                MTDM.cdsCustomerGroup.UpdateOptions.UpdateTableName);
+//
+//              if not MTDM.cdsCustomerGroup.Locate('ID', ID, []) then
+//                MTDM.cdsCustomerGroup.First;
               end;
 
             ftCustomerStatus:
               begin
                 DataSet := MTDM.cdsCustomerStatus;
                 ScriptID := 14;
+                FileName := 'C:\Data\Xml\Customer Status.xml';
+
+//              ID := MTDM.cdsCustomerStatus.FieldByName('ID').AsInteger;
+//
+//              VBBaseDM.GetData(14, MTDM.cdsCustomerStatus, MTDM.cdsCustomerStatus.Name, '',
+//                'C:\Data\Xml\Age Period.xml', MTDM.cdsCustomerStatus.UpdateOptions.Generatorname,
+//                MTDM.cdsCustomerStatus.UpdateOptions.UpdateTableName);
+//
+//              if not MTDM.cdsCustomerStatus.Locate('ID', ID, []) then
+//                MTDM.cdsCustomerStatus.First;
               end;
 
             ftCustomerType:
@@ -190,6 +254,15 @@ begin
                 DataSet := MTDM.cdsCustomerType;
                 ScriptID := 15;
                 FileName := 'C:\Data\Xml\Customer Type.xml';
+
+//              ID := MTDM.cdsCustomerType.FieldByName('ID').AsInteger;
+//
+//              VBBaseDM.GetData(15, MTDM.cdsCustomerType, MTDM.cdsCustomerType.Name, '',
+//                'C:\Data\Xml\Age Period.xml', MTDM.cdsCustomerType.UpdateOptions.Generatorname,
+//                MTDM.cdsCustomerType.UpdateOptions.UpdateTableName);
+//
+//              if not MTDM.cdsCustomerType.Locate('ID', ID, []) then
+//                MTDM.cdsCustomerType.First;
               end;
 
             ftJobFunction:
@@ -197,6 +270,15 @@ begin
                 DataSet := MTDM.cdsJobFunction;
                 ScriptID := 19;
                 FileName := 'C:\Data\Xml\Job Function.xml';
+
+//              ID := MTDM.cdsJobFunction.FieldByName('ID').AsInteger;
+//
+//              VBBaseDM.GetData(19, MTDM.cdsJobFunction, MTDM.cdsJobFunction.Name, '',
+//                'C:\Data\Xml\Age Period.xml', MTDM.cdsJobFunction.UpdateOptions.Generatorname,
+//                MTDM.cdsJobFunction.UpdateOptions.UpdateTableName);
+//
+//              if not MTDM.cdsJobFunction.Locate('ID', ID, []) then
+//                MTDM.cdsJobFunction.First;
               end;
 
             ftMonthOfYear:
@@ -204,6 +286,15 @@ begin
                 DataSet := MTDM.cdsMonthOfYear;
                 ScriptID := 20;
                 FileName := 'C:\Data\Xml\Month of Year.xml';
+
+//              ID := MTDM.cdsMonthOfYear.FieldByName('ID').AsInteger;
+//
+//              VBBaseDM.GetData(20, MTDM.cdsMonthOfYear, MTDM.cdsMonthOfYear.Name, '',
+//                'C:\Data\Xml\Age Period.xml', MTDM.cdsMonthOfYear.UpdateOptions.Generatorname,
+//                MTDM.cdsMonthOfYear.UpdateOptions.UpdateTableName);
+//
+//              if not MTDM.cdsMonthOfYear.Locate('ID', ID, []) then
+//                MTDM.cdsMonthOfYear.First;
               end;
 
             ftPricelist:
@@ -211,6 +302,15 @@ begin
                 DataSet := MTDM.cdsPricelist;
                 ScriptID := 22;
                 FileName := 'C:\Data\Xml\Price List.xml';
+
+//              ID := MTDM.cdsPricelist.FieldByName('ID').AsInteger;
+//
+//              VBBaseDM.GetData(22, MTDM.cdsPricelist, MTDM.cdsPricelist.Name, ONE_SPACE,
+//                'C:\Data\Xml\Price List.xml', MTDM.cdsPricelist.UpdateOptions.Generatorname,
+//                MTDM.cdsPricelist.UpdateOptions.UpdateTableName);
+//
+//              if not MTDM.cdsPricelist.Locate('ID', ID, []) then
+//                MTDM.cdsPricelist.First;
               end;
 
             ftRateUnit:
@@ -218,6 +318,15 @@ begin
                 DataSet := MTDM.cdsRateUnit;
                 ScriptID := 38;
                 FileName := 'C:\Data\Xml\Rate Unit.xml';
+
+//              ID := MTDM.cdsRateUnit.FieldByName('ID').AsInteger;
+//
+//              VBBaseDM.GetData(38, MTDM.cdsRateUnit, MTDM.cdsRateUnit.Name, '',
+//                'C:\Data\Xml\Age Period.xml', MTDM.cdsRateUnit.UpdateOptions.Generatorname,
+//                MTDM.cdsRateUnit.UpdateOptions.UpdateTableName);
+//
+//              if not MTDM.cdsRateUnit.Locate('ID', ID, []) then
+//                MTDM.cdsRateUnit.First;
               end;
 
             ftSalutation:
@@ -225,6 +334,15 @@ begin
                 DataSet := MTDM.cdsSalutation;
                 ScriptID := 23;
                 FileName := 'C:\Data\Xml\Salutation.xml';
+
+//              ID := MTDM.cdsSalutation.FieldByName('ID').AsInteger;
+//
+//              VBBaseDM.GetData(23, MTDM.cdsSalutation, MTDM.cdsSalutation.Name, '',
+//                'C:\Data\Xml\Age Period.xml', MTDM.cdsSalutation.UpdateOptions.Generatorname,
+//                MTDM.cdsSalutation.UpdateOptions.UpdateTableName);
+//
+//              if not MTDM.cdsSalutation.Locate('ID', ID, []) then
+//                MTDM.cdsSalutation.First;
               end;
 
             ftStdActivity:
@@ -232,6 +350,15 @@ begin
                 DataSet := MTDM.cdsStdActivity;
                 ScriptID := 52;
                 FileName := 'C:\Data\Xml\Standard Activity.xml';
+
+//              ID := MTDM.cdsStdActivity.FieldByName('ID').AsInteger;
+//
+//              VBBaseDM.GetData(52, MTDM.cdsStdActivity, MTDM.cdsStdActivity.Name, '',
+//                'C:\Data\Xml\Age Period.xml', MTDM.cdsStdActivity.UpdateOptions.Generatorname,
+//                MTDM.cdsStdActivity.UpdateOptions.UpdateTableName);
+//
+//              if not MTDM.cdsStdActivity.Locate('ID', ID, []) then
+//                MTDM.cdsStdActivity.First;
               end;
 
             ftTaxoffice:
@@ -239,6 +366,15 @@ begin
                 DataSet := MTDM.cdsTaxOffice;
                 ScriptID := 25;
                 FileName := 'C:\Data\Xml\Tax Office.xml';
+
+//              ID := MTDM.cdsTaxOffice.FieldByName('ID').AsInteger;
+//
+//              VBBaseDM.GetData(25, MTDM.cdsTaxOffice, MTDM.cdsTaxOffice.Name, '',
+//                'C:\Data\Xml\Age Period.xml', MTDM.cdsTaxOffice.UpdateOptions.Generatorname,
+//                MTDM.cdsTaxOffice.UpdateOptions.UpdateTableName);
+//
+//              if not MTDM.cdsTaxOffice.Locate('ID', ID, []) then
+//                MTDM.cdsTaxOffice.First;
               end;
 
             ftVehicleMake:
@@ -246,6 +382,15 @@ begin
                 DataSet := MTDM.cdsVehicleMake;
                 ScriptID := 48;
                 FileName := 'C:\Data\Xml\Vehicle Make.xml';
+
+//              ID := MTDM.cdsVehicle.FieldByName('ID').AsInteger;
+//
+//              VBBaseDM.GetData(48, MTDM.cdsVehicle, MTDM.cdsVehicle.Name, '',
+//                'C:\Data\Xml\Age Period.xml', MTDM.cdsVehicle.UpdateOptions.Generatorname,
+//                MTDM.cdsVehicle.UpdateOptions.UpdateTableName);
+//
+//              if not MTDM.cdsVehicle.Locate('ID', ID, []) then
+//                MTDM.cdsVehicle.First;
               end;
           end;
 
@@ -299,7 +444,7 @@ begin
   end;
 end;
 
-procedure TBaseGridFrm.SetButtonVisibility(DataSet: TFDMemTable;  MasterID: Integer);
+procedure TBaseGridFrm.SetButtonVisibility(DataSet: TFDMemTable; MasterID: Integer);
 var
   ReadOnly: Boolean;
 begin
